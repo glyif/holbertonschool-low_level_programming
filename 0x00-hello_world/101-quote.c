d2e44f97ec83dcc9c3e7c76a7051ec092b3af866
Bobby Yang <122@holbertonschool.com>
2017-02-08 20:54:16 +0000

    101 try 1

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <unistd.h>
+/*
+* 101-quote.c
+* Description: This program takes a string and prints it
+* using write.
+*/
+
+/**
+* main - The main function running everything.
+* @void: not taking an arguments.
+*
+* Return: 1
+*/
+
+int main(void)
+{
+	write(1,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
