e7a138c946e324fe79b1d9ecd148b40b6e25cd5e
Bobby Yang <122@holbertonschool.com>
2017-02-08 21:01:18 +0000

    edited max bytes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -15,6 +15,6 @@
 
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
 	return (1);
 }
