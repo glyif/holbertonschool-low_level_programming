b16d9af29280f7f2b3bfecf13468002ed5b755dd
Bobby Yang <122@holbertonschool.com>
2017-02-14 17:46:43 +0000

    8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -4,6 +4,7 @@ void jack_bauer(void)
 {
 	int i, j, k, n;
 
+	
 	for (i = 0; i <= 2; i++)
 	{
 		for (j = 0; j <= 9; j++)
@@ -12,12 +13,19 @@ void jack_bauer(void)
 			{
 				for (n = 0; n <= 9; n++)
 				{
-					_putchar(i + '0');
-					_putchar(j + '0');
-					_putchar(':');
-					_putchar(k + '0');
-					_putchar(n + '0');
-					_putchar('\n');
+					if (i == 2 && j == 4 && k == 0 && n == 0)
+					{
+						return;
+					}
+					else
+					{
+						_putchar(i + '0');
+						_putchar(j + '0');
+						_putchar(':');
+						_putchar(k + '0');
+						_putchar(n + '0');
+						_putchar('\n');
+					}
 				}
 			}
 		}
