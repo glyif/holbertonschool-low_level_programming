a149f36c2e0c618d5563773e119362853a285e0e
Bobby Yang <122@holbertonschool.com>
2017-02-14 20:02:32 +0000

    try 2

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -8,16 +8,15 @@
 
 int main(void)
 {
-	int i = 0;
-
-	char s[10] = "Holberton";
-
-	for (i = 0; i <= 9; i++)
-	{
-		_putchar(s[i]);
-	}
-
-	_putchar('\n');
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
 
 	return (0);
 }
