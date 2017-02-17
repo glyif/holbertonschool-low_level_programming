1d03733e76d0a99e4b46237f28e4b6d6fdfaf3f3
Bobby Yang <122@holbertonschool.com>
2017-02-17 04:51:14 +0000

    try again

--- 0x03-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,11 +17,14 @@ int main(void)
 		if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz ");
 		else if(i % 3  == 0)
-			printf("Fizz ");	
+			printf("Fizz");	
 		else if (i % 5 == 0)
-			printf("Buzz ");
+			printf("Buzz");
 		else
 			printf("%d ", i);
+
+		if (i < 100)
+			printf(" ");
 	}
 	
 	putchar('\n');
