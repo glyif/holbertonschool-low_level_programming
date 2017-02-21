d487c9e2708574bf14eede43b45f12bb78dc91c0
Bobby Yang <122@holbertonschool.com>
2017-02-21 20:12:21 +0000

    try again

--- 0x04-pointers_arrays_strings/7-puts_half.c
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -10,10 +10,17 @@ void puts_half(char *str)
 {
 	int i;
 
-	for (i = 1; str[i] != '\0'; i++)
-		;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+
 	i++;
+
 	for (i = i / 2; str[i] != '\0'; i++)
+	{
 		_putchar(str[i]);
+	}
+
 	_putchar('\n');
 }
