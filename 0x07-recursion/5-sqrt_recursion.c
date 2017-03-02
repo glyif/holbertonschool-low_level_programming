051744b1c63d8ddb7475ce911dc484b429e40a62
Bobby Yang <122@holbertonschool.com>
2017-03-02 00:52:49 +0000

    betty checked

--- 0x07-recursion/5-sqrt_recursion.c
+++ 0x07-recursion/5-sqrt_recursion.c
@@ -1,26 +1,17 @@
 /**
- * _sqrt_recursion - square root of a number
- * @n: number
+ * _find_natural - finds natural sqrt
+ * @a: number to find sqrt
+ * @b: sqrt number
  *
- * Return: square root of the n, if no square root, -1
+ * Return: depends
  */
-
-int _find_natural(int a, int b);
-
-int _sqrt_recursion(int n)
-{
-	
-	return(_find_natural(n, 1));
-	
-
-}
 
 int _find_natural(int a, int b)
 {
 	if (a < 0)
 		return (-1);
 	else if (a == 0)
-		return (0);
+		return (1);
 
 	if (b * b > a)
 		return (-1);
@@ -28,7 +19,19 @@ int _find_natural(int a, int b)
 		return (b);
 	else
 		return (_find_natural(a, b + 1));
+}
+
+/**
+ * _sqrt_recursion - square root of a number
+ * @n: number
+ *
+ * Return: square root of the n, if no square root, -1
+ */
+
+int _sqrt_recursion(int n)
+{
+
+	return (_find_natural(n, 1));
+
 
 }
-	
-	
