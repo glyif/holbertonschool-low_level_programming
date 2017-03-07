5178f2b4601902adcd89f2c5eb75fba10087fbf5
Bobby Yang <122@holbertonschool.com>
2017-03-07 23:29:49 +0000

    2 with 0

---
+++ 0x0A-malloc_free/5-argstostr.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * argstostr - concats all arguments
+ * @ac: count of arguments
+ * @av: arguments
+ *
+ * Return: pointer to new string or NULL
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int len;
+
+}
