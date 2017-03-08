0c96235e9e76b05ce2ec6c6e16634c483b4d03d6
Bobby Yang <122@holbertonschool.com>
2017-03-08 05:29:24 +0000

    5

--- 0x0A-malloc_free/5-argstostr.c
+++ 0x0A-malloc_free/5-argstostr.c
@@ -11,6 +11,35 @@
 
 char *argstostr(int ac, char **av)
 {
-	int len;
+	char *ar;
+	int i, j, k, l;
 
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0, l = 0, k = 0; k < ac; i++, l++)
+	{
+		if (av[k][i]  == '\0')
+			k++;
+	}
+	
+	l++;
+
+	ar = malloc(l * sizeof(char));
+	
+	if (ar == NULL)
+		return (NULL);
+
+	for (i = 0, j = 0, k = 0; k < ac; i++, j++)
+	{
+		ar[j] = av[k][i];
+		if (av[k][i] == '\0')
+		{
+			ar[j] = '\n';
+			k++;
+			i = -1;
+		}
+	}
+
+	return (ar);
 }
