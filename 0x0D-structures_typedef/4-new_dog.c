aea2073795570a5d0c9380f8f85ca66e48cdea8b
Bobby Yang <122@holbertonschool.com>
2017-03-13 20:01:06 +0000

    try 4

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -12,5 +12,31 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	
+	unsigned int i, name_len, owner_len;
+	dog_t *new_dog;
+
+	for (name_len = 0; name[name_len] != '\0'; name_len++)
+		;
+
+	for (owner_len = 0; owner[owner_len] != '\0'; owner_len++)
+		;
+
+	new_dog = malloc(sizeof(dog_t));
+
+	if (new_dog == NULL)
+		return (NULL);
+
+	new_dog->name = malloc(name_len);
+	new_dog->owner = malloc(owner_len);
+
+	for (i = 0; name[i] != '\0'; i++)
+		new_dog->name[i] = name[i];
+
+	for (i = 0; owner[i] != '\0'; i++)
+		new_dog->owner[i] = owner[i];
+
+	new_dog->age = age;
+
+	return(new_dog);
+		
 }
