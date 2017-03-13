b30446e8fbb888cb750d96fd88f16874243b626b
Bobby Yang <122@holbertonschool.com>
2017-03-13 17:44:17 +0000

    init

---
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+/**
+ * init_dog - initializes dog
+ * @d: structure dog
+ * @name: name
+ * @age: age as float
+ * @owner: owner name
+ *
+ * Return: nothing
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		;
+	else
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
---
+++ 0x0D-structures_typedef/README.md
@@ -0,0 +1,3 @@
+#Structures
+
+##creating and using structure
