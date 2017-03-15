e7f2746a9b15fd3153228427c7bdd1e02e2d9284
glyif <122@holbertonschool.com>
2017-03-15 11:31:57 -0700

    removed unnecessary checks

--- 0x0E-function_pointers/3-main.c
+++ 0x0E-function_pointers/3-main.c
@@ -24,7 +24,7 @@ int main(int argc, char **argv)
 
 	calc = (get_op_func(argv[2]));
 
-	if (calc == NULL || argv[2] == NULL || *argv[2] == '\0')
+	if (calc == NULL)
 	{
 		printf("Error\n");
 		exit(99);
