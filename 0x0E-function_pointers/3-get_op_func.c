4b96831e770c89fe422230694491269d53b5b1da
glyif <122@holbertonschool.com>
2017-03-15 15:34:55 -0700

    removed strncmpy

--- 0x0E-function_pointers/3-get_op_func.c
+++ 0x0E-function_pointers/3-get_op_func.c
@@ -27,7 +27,7 @@ int (*get_op_func(char *s))(int a, int b)
 
 	while (ops[i].op != NULL)
 	{
-		if ((strncmp(s, ops[i].op, strlen(ops[i].op))) == 0)
+		if ((strcmp(s, ops[i].op)) == 0)
 			return (ops[i].f);
 
 		i++;
