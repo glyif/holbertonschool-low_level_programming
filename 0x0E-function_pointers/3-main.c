14e868db717d49685727fafe958b395aa3e65b64
glyif <122@holbertonschool.com>
2017-03-15 10:49:14 -0700

    added null checker

--- 0x0E-function_pointers/3-main.c
+++ 0x0E-function_pointers/3-main.c
@@ -13,6 +13,8 @@
 int main(int argc, char **argv)
 {
 	int (*calc)(int, int);
+	int a;
+	int b;
 
 	if (argc != 4)
 	{
@@ -22,13 +24,16 @@ int main(int argc, char **argv)
 
 	calc = (get_op_func(argv[2]));
 	
-	if (calc == NULL)
+	if (calc == NULL || argv[2] == NULL)
 	{
 		printf("Error\n");
 		return (0);
 	}
 
-	printf("%d\n", calc(atoi(argv[1]), atoi(argv[3])));
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	printf("%d\n", calc(a, b));
 
 	return (0);
 }
