44a939d484b7efbb5085a0f98e85298e60dcf0c8
Bobby Yang <122@holbertonschool.com>
2017-04-04 19:27:24 +0000

    updated with compileable code

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -8,7 +8,7 @@
  * Return: total number of chars printed.
  */
 
-ssize_t read_textfile(const char *filename, size_t letters);
+ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *to_print;
 	size_t rd, wt;
