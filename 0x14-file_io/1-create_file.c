c4570c4de88f3cd06c5a691901091e01bef2e727
Bobby Yang <122@holbertonschool.com>
2017-04-04 20:44:19 +0000

    updated without close checls

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -38,10 +38,6 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	if (close(file) < 0)
-		return (-1);
-
+	close(file)
 	return (1);
-
-
 }
--- 0x14-file_io/2-append_text_to_file.c
+++ 0x14-file_io/2-append_text_to_file.c
@@ -39,9 +39,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	}
 	
 
-	if (close(file) < 0)
-		return (-1);
-
+	close(file);
 	return (1);
 
 }
