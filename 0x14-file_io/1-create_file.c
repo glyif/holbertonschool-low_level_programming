56d3d731d6ba616c0a56dae015029cb626a6bbeb
Bobby Yang <122@holbertonschool.com>
2017-04-04 20:10:06 +0000

    removed null

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -19,8 +19,6 @@ int create_file(const char *filename, char *text_content)
 	for (size = 0; text_content[size] != '\0'; size++)
 		;
 
-	size++;
-
 	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 
 	if (file < 0)
