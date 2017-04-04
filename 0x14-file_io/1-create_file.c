7507f174d0d3b09536911be4aecb514322706a82
Bobby Yang <122@holbertonschool.com>
2017-04-04 19:47:18 +0000

    updated task 1 with checks

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -11,7 +11,7 @@
 int create_file(const char *filename, char *text_content)
 {
 	int file, size;
-	size_t wt;
+	ssize_t wt;
 
 	if (filename == NULL)
 		return (-1);
@@ -24,7 +24,7 @@ int create_file(const char *filename, char *text_content)
 	if (file < 0)
 		return (-1);
 
-	if (size == 0)
+	if (text_content == NULL)
 	{
 		close(file);
 		return (1);
@@ -32,7 +32,7 @@ int create_file(const char *filename, char *text_content)
 
 	wt = write(file, text_content, size);
 
-	if (wt <= 0)
+	if (wt < 0)
 	{
 		close(file);
 		return (-1);
