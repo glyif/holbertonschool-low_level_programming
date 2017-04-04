90e703410dc524690ba583936efd91d6494c20ad
Bobby Yang <122@holbertonschool.com>
2017-04-04 17:20:32 +0000

    initial commit with task 1

---
+++ 0x14-file_io/0-read_textfile.c
@@ -0,0 +1,59 @@
+#include "holberton.h"
+
+/**
+ * read_textfile - reads a text file and prints it to standard output
+ * @filename: relative or absolute path of the file
+ * @letters: number of letters to read and print
+ *
+ * Return: total number of chars printed.
+ */
+
+size_t read_textfile(const char *filename, size_t letters)
+{
+	char *to_print;
+	size_t rd, wt;
+	int file;
+
+	if (filename == NULL)
+		return (0);
+
+	to_print = malloc(sizeof(char) * letters);
+	
+	if (to_print == NULL)
+		return (0);
+
+	file = open(filename, O_RDONLY);
+
+	if (file < 0)
+	{
+		free(to_print);
+		return (0);
+	}
+
+	rd = read(file, to_print, letters);
+
+	if (rd < 0)
+	{
+		free(to_print);
+		return (0);
+	}
+
+	wt = write(STDIN_FILENO, to_print, letters);
+
+	if (wt < rd)
+	{
+		free(to_print);
+		return (0);
+	}
+
+	if(close(file) < 0)
+	{
+		free(to_print);
+		return (0);
+	}
+
+	free(to_print);
+
+	return (rd);
+
+}
---
+++ 0x14-file_io/holberton.h
@@ -0,0 +1,7 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+size_t read_textfile(const char *filename, size_t letters);
+#endif
