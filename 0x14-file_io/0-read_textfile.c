8b324830d210da537eb249f784b7ff32e94a745e
Bobby Yang <122@holbertonschool.com>
2017-04-04 19:14:17 +0000

    betty checked

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -16,44 +16,32 @@ size_t read_textfile(const char *filename, size_t letters)
 
 	if (filename == NULL)
 		return (0);
-
 	to_print = malloc(sizeof(char) * letters);
-	
 	if (to_print == NULL)
 		return (0);
-
 	file = open(filename, O_RDONLY);
-
 	if (file < 0)
 	{
 		free(to_print);
 		return (0);
 	}
-
 	rd = read(file, to_print, letters);
-
 	if (rd <= 0)
 	{
 		free(to_print);
 		return (0);
 	}
-
 	wt = write(STDIN_FILENO, to_print, letters);
-
 	if (wt < rd)
 	{
 		free(to_print);
 		return (0);
 	}
-
-	if(close(file) < 0)
+	if (close(file) < 0)
 	{
 		free(to_print);
 		return (0);
 	}
-
 	free(to_print);
-
 	return (rd);
-
 }
