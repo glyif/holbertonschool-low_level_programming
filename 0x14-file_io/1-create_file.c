4198c5cde390addb004b01e294b0c0802220520a
Bobby Yang <122@holbertonschool.com>
2017-04-05 00:04:52 +0000

    updated with -1 and flow

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -16,12 +16,9 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	for (size = 0; text_content[size] != '\0'; size++)
-		;
-
 	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 
-	if (file < 0)
+	if (file == -1)
 		return (-1);
 
 	if (text_content == NULL)
@@ -29,10 +26,12 @@ int create_file(const char *filename, char *text_content)
 		close(file);
 		return (1);
 	}
+	for (size = 0; text_content[size] != '\0'; size++)
+		;
 
 	wt = write(file, text_content, size);
 
-	if (wt < 0)
+	if (wt == -1)
 	{
 		close(file);
 		return (-1);
